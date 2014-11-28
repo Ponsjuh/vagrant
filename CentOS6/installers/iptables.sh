b54c23f27770ba817cc1af9282c83d71c0382929
Frank Pons <f.pons@zetacom.nl>
2014-11-28 12:58:16 +0100

    Fixed Labe;s

--- CentOS6/installers/iptables.sh
+++ CentOS6/installers/iptables.sh
@@ -5,7 +5,7 @@ INSTALLED=$(iptables -L | grep http | wc -l);
 if [ $INSTALLED -eq 0 ]; then
     echo '   > [CONFIG  ] Adding port 80 to INPUT chain';
     iptables -I INPUT 5 -p tcp -m tcp --dport 80 -j ACCEPT
-    service iptables save;
+    service iptables save > /dev/null;
 else
     echo '   > [SKIPPING ] Iptables';
 fi
--- CentOS6/installers/php.sh
+++ CentOS6/installers/php.sh
@@ -3,15 +3,15 @@
 echo '---[Installing PHP]---';
 INSTALLED=$(yum info php | grep Repo | sed -n "/\installed/p" | wc -l)
 if [ $INSTALLED -eq 0 ]; then
-    echo '   > [INSTALL  ] PHP';
+    echo '   > [INSTALL ] PHP';
     yum -y -q --enablerepo=remi install php php-mysql php-mbstring php-devel php-gd php-pecl-xdebug php-pspell php-snmp php-odbc php-xmlrpc php-xml php-imap php-mssql php-ldap php-mcrypt > /dev/null;
 
-    echo '   > [CONFIG   ] PHP Error Reporting';
+    echo '   > [CONFIG  ] PHP Error Reporting';
     sed -i 's|error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = E_ALL|' /etc/php.ini
     sed -i 's|display_errors = Off|display_errors = On|' /etc/php.ini
 
-    echo '   > [CONFIG   ] PHP Timezone';
+    echo '   > [CONFIG  ] PHP Timezone';
     sed -i 's|;date.timezone =|date.timezone = "Europe/Amsterdam"|' /etc/php.ini
 else
-    echo '   > [SKIPPING ] PHP';
+    echo '   > [SKIPPING] PHP';
 fi
