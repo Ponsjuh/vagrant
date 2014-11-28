36416476e4cf40dacfa3af142fb6cf8566a8ad4a
Frank Pons <f.pons@zetacom.nl>
2014-11-28 12:11:56 +0100

    added remi repos

--- CentOS6/installers/extraRepos.sh
+++ CentOS6/installers/extraRepos.sh
@@ -5,6 +5,10 @@ if [ $INSTALLED -eq 0 ]; then
       echo '---[Installing extra repositories]---';
       yum -q -y install epel-release
       rpm --import --quiet /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 > /dev/null;
+
+      rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+      rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
+
 else
     echo '---[Already installed extra repositories]--';
 fi
