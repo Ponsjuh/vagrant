1292bee512c03b57fe8a97d27cd154600abf1916
Frank Pons <f.pons@zetacom.nl>
2014-11-28 12:16:42 +0100

    updated repos installer

--- CentOS6/installers/extraRepos.sh
+++ CentOS6/installers/extraRepos.sh
@@ -2,15 +2,20 @@
 
 INSTALLED=$(yum repolist | grep epel | wc -l)
 if [ $INSTALLED -eq 0 ]; then
-      echo '---[Installing extra repositories]---';
+      echo '----[Installing epel repositorie]---';
       yum -q -y install epel-release
       rpm --import --quiet /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 > /dev/null;
-
-      rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-      rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
-
 else
-    echo '---[Already installed extra repositories]--';
+    echo '---[Already installed epel repositories]--';
+fi
+
+INSTALLED=$(yum repolist | grep remi | wc -l)
+if [ $INSTALLED -eq 0 ]; then
+      echo '----[Installing remi repositorie]---';
+      rpm -Uvh --quiet --nosignature --nodigest http://rpms.famillecollet.com/enterprise/remi-release-6.rpm > /dev/null;
+      rpm --quiet --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi  > /dev/null;
+else
+    echo '----[Already installed remi repositorie]--';
 fi
 
 echo '---[Updating OS]---';
