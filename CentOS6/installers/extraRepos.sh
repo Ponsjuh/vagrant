9dafd154849e9f87d4b2407b4c89ad5f61a10a41
Frank Pons <f.pons@zetacom.nl>
2014-11-28 12:34:51 +0100

    fixed label in extrarepos

--- CentOS6/installers/extraRepos.sh
+++ CentOS6/installers/extraRepos.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
+echo '---[Installing extra repositories]---';
 INSTALLED=$(yum repolist | grep epel | wc -l)
 if [ $INSTALLED -eq 0 ]; then
-      echo '----[Installing epel repositorie]---';
+      echo '----[Epel repository]---';
       yum -q -y install epel-release
       rpm --import --quiet /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 > /dev/null;
 else
-    echo '---[Already installed epel repositories]--';
+    echo '---[Already installed epel repository]--';
 fi
 
 INSTALLED=$(yum repolist | grep remi | wc -l)
 if [ $INSTALLED -eq 0 ]; then
-      echo '----[Installing remi repositorie]---';
+      echo '----[Remi repository]---';
       rpm -Uvh --quiet --nosignature --nodigest http://rpms.famillecollet.com/enterprise/remi-release-6.rpm > /dev/null;
-      rpm --quiet --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi  > /dev/null;
+      rpm --quiet --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi > /dev/null;
 else
-    echo '----[Already installed remi repositorie]--';
+    echo '----[Already installed remi repository]--';
 fi
 
 echo '---[Updating OS]---';
